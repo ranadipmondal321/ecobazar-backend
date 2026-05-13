cb22064e896584f867cd239c7ad81d5d6568a681
ranadipmondal321 <ranadipmondal321@gmail.com>
2026-05-13 15:37:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    https://ecobazar-frontend1.onrender.com
+    "https://ecobazar-frontend1.onrender.com"
   ],
   credentials: true
 }));
